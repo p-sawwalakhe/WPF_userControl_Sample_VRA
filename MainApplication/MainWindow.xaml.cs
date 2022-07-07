e6ebc554a6e4dafbae0fafa26e79ab81b5e61932
sawwalakhepritam <sawwalakhepritam@gmail.com>
2022-07-07 14:57:37 +0530

    Complete imnplementation for sample

---
+++ MainApplication/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainApplication
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			usercontrol1.txtNumberTextChanged += new TextChangedEventHandler(txtNumber_TextChanged);
+			usercontrol2.txtNumberTextChanged += new TextChangedEventHandler(txtNumber1_TextChanged);
+			usercontrol3.txtNumberTextChanged += new TextChangedEventHandler(txtNumber2_TextChanged);
+		}
+
+		private void txtNumber_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			MessageBox.Show(usercontrol1.SelectedNumber);
+		}
+
+		private void txtNumber1_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			MessageBox.Show(usercontrol2.SelectedNumber);
+		}
+
+		private void txtNumber2_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			MessageBox.Show(usercontrol3.SelectedNumber);
+		}
+	}
+}
---
+++ MainApplication/NumberUserControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainApplication
+{
+	/// <summary>
+	/// Interaction logic for NumberUserControl.xaml
+	/// </summary>
+	public partial class NumberUserControl : UserControl
+	{
+		public string SelectedNumber
+		{
+			get { return txtNumber.Text; }
+		}
+
+		public event TextChangedEventHandler txtNumberTextChanged;
+
+		public NumberUserControl()
+		{
+			InitializeComponent();
+			txtNumber.TextChanged += new TextChangedEventHandler(txtNumber_TextChanged);
+		}
+
+		private void clickMe_Click(object sender, RoutedEventArgs e)
+		{
+			txtNumber.Text = (Convert.ToInt32(txtNumber.Text ?? "0") + 1).ToString();
+			//MessageBox.Show("Clicked !");
+		}
+
+		private void txtNumber_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (txtNumberTextChanged!=null)
+			{
+				txtNumberTextChanged(sender, e);
+			}
+			
+		}
+	}
+}
